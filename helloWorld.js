37b44d24b84cd898af363aed15758fda077af6d0
Mingliang-Xu <oliver.xml@hotmail.com>
2023-09-12 15:07:54 -0700

    Finished the demo

---
+++ helloWorld.js
@@ -0,0 +1,22 @@
+const firstName = "Oliver";
+let age = 36;
+console.log(firstName, age);
+let heightInFeet = 5;
+let inches = 2;
+height = heightInFeet * 12;
+console.log(height);
+height += inches;
+console.log(height);
+let isCool = true;
+let tempLit = `Hi, my name is ${firstName}! I am ${age} years old and it is ${isCool} that I am cool.`;
+let string = "Hi, my name is ${firstName}! I am ${age} years old and it is ${isCool} that I am cool.";
+console.log(tempLit);
+console.log(string);
+let score = 102;
+let score2 = 115; 
+if(score > score2){
+    console.log("First score is bigger");
+} else {
+    console.log("Second is bigger");
+};
+// === ; !=//
